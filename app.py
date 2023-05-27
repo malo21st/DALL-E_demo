f301326f140277030f2366179041e6faa9f26c1f
malo21st <44739330+malo21st@users.noreply.github.com>
2023-05-28 01:30:46 +0900

    Update app.py

--- app.py
+++ app.py
@@ -49,7 +49,7 @@ def image_edit(prompt):
 
 prompt_create = st.sidebar.text_input('**prompt**', "")
 
-if st.session_state["create"]["is_img"] == False:
+if st.session_state["create"]["is_img"] == False and prompt_create:
     im_create = image_create(prompt_create)
     st.session_state["create"] = {"is_img": True, "img": im_create}
 if st.session_state["create"]["is_img"]:
